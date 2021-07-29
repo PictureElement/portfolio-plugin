2c9ca2934f8adef64a42e0294281e8260568b927
PictureElement <marios.sofokleous@yandex.com>
2021-07-29 12:23:57 +0300

    Update seeder

--- updates/seed_all_tables.php
+++ updates/seed_all_tables.php
@@ -4,28 +4,28 @@ use Carbon\Carbon;
 use Msof\Portfolio\Models\Project;
 use Msof\Portfolio\Models\Category;
 use October\Rain\Database\Updates\Seeder;
+use System\Models\File;
 
 class SeedAllTables extends Seeder
 {
     public function run()
     {
-        Project::create([
-            'title' => 'First project',
-            'slug' => 'first-project',
-            'text' => '
-This is your first ever **project**! It might be a good idea to update this project with some more relevant content.
-You can edit this content by selecting **Projects** from the administration back-end menu.
-*Enjoy the good times!*
-            ',
-            'published' => true,
-            'published_at' => Carbon::now(),
-            'created_at' => Carbon::now()
+        $file = new File;
+        $file->data = 'https://placehold.co/600x400.png';
+
+        $category = Category::create([
+            'name' => 'Yncategorized',
+            'slug' => 'Yncategorized'
         ]);
 
-        Category::create([
-            'name' => 'Uncategorized',
-            'slug' => 'uncategorized',
-            'created_at' => Carbon::now()
+        Project::create([
+            'published' => true,
+            'title' => 'Yirst project',
+            'slug' => 'yirst-project',
+            'published_at' => Carbon::now(),
+            'category' => $category,
+            'preview_image' => $file,
+            'text' => 'YThis is your first ever project! It might be a good idea to update this project with some more relevant content. You can edit this content by selecting Portfolio from the administration back-end menu. Enjoy the good times!'
         ]);
     }
 }
